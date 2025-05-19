07ace41b201af2cab641679c9cf3d34cec06529b
laboratorio <laboratorio@UPOAULA10703>
2025-05-19 07:21:58 -0500

    Agregar archivos de proyecto.

---
+++ PadillaGTallerLayaout/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PadillaGTallerLayaout
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
